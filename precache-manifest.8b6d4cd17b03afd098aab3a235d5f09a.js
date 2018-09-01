9bcdc4c5915f8a4e5c747c181333fba1bb4cf4ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-01 08:01:18 +0000

    Deploy HZFE/Notablog to github.com/HZFE/Notablog.git:gh-pages

---
+++ precache-manifest.8b6d4cd17b03afd098aab3a235d5f09a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ebe10564e00abf8d91d0",
+    "url": "/js/chunk-vendors.4f4f2361.js"
+  },
+  {
+    "revision": "b75ef5dcd3684e16ff2b",
+    "url": "/js/app.dbedcf60.js"
+  },
+  {
+    "revision": "083b480c704835c0eac2",
+    "url": "/js/about.5d688836.js"
+  },
+  {
+    "revision": "bbd13f718046b691fe93a59686a0829f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b75ef5dcd3684e16ff2b",
+    "url": "/css/app.c47f4ae6.css"
+  }
+];
